a9a5d6c52eb77fa3e3e544b56df36bda5fc15deb
Amirelect <amirelect.graph@gmail.com>
2026-05-10 18:44:31 +0330

    create single, update, delete for post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import HomeView, NewPostView
+from .views import (HomeView, 
+                    PostNewView, 
+                    PostDetailView, 
+                    PostUpdateView, 
+                    PostDeleteView)
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('post/new', NewPostView.as_view(), name='newpost')
+    path('post/new', PostNewView.as_view(), name='post_new'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/update/<int:pk>', PostUpdateView.as_view(), name='update'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import (CreateView, 
+                                  ListView, 
+                                  DetailView, 
+                                  UpdateView, 
+                                  DeleteView)
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Post
     template_name = 'home.html'
     
-class NewPostView(CreateView):
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_single.html'
+    
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'excerpt', 'body', 'photo']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+    
+class PostNewView(CreateView):    
     template_name = 'post_new.html'
     model = Post
     success_url = reverse_lazy('home')
